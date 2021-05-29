9771e045611abfdc499708d48bbaa31d47aea642
pro1216 <shotashota73@outlook.com>
2021-05-30 07:51:23 +0900

    リファクタリング

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -8,49 +8,54 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 
+
+
 class AuthController extends Controller
 {
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
     /**
      * @return View
      */
-    
-    public function showLogin(){
+
+    public function showLogin()
+    {
         return view('login.login_form');
     }
 
     /**
      * @param App\Http\Requests\LoginFormRequest
-     * $request
+     * $return
      */
-    public function login(LoginFormRequest $request){
+    public function login(LoginFormRequest $request)
+    {
 
         $credentials = $request->only('email', 'password');
 
         //アカウントがロックされていたらはじく
-        $user = User::where('email','=',$credentials['email']
-        )->first();
-        if(!is_null($user)){
-            if($user->locked_flg ===1){
+        $user = $this->user->getUserByEmail($credentials['email']);
+        if (!is_null($user)) {
+            if ($this->user->isAccountLocked($user)) {
+
                 return back()->withErrors([
                     'danger' => 'アカウントがロックされています'
                 ]);
             }
-            if(Auth::attempt($credentials)){
+            if (Auth::attempt($credentials)) {
                 $request->session()->regenerate();
 
                 //ログイン成功時error_acountを0にする
-                if($user->error_count > 0){
-                    $user->error_count = 0;
-                    $user->save();
-                }
+                $this->user->resetErrorCount($user);
+
                 return redirect()->route('home')
-                ->with('success','ログイン成功');
+                    ->with('success', 'ログイン成功');
             }
             //ログインｓ失敗時エラーアカウントを1増やす
-            $user->error_count = $user->error_count + 1;
-            if($user->error_count > 5){
-                $user->locked_flg=1;
-                $user->save();
+            $user->error_account =
+                $this->user->addErrorAccount($user->error_account);
+            if ($this->user->lockAccount($user)) {
                 return back()->withErrors([
                     'danger' => 'アカウントがロックされました。'
                 ]);
@@ -67,7 +72,8 @@ class AuthController extends Controller
      * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Request
      */
-    public function logout(Request $request){
+    public function logout(Request $request)
+    {
         Auth::logout();
 
         $request->session()->invalidate();
@@ -75,6 +81,6 @@ class AuthController extends Controller
         $request->session()->regenerateToken();
 
         return redirect()->route('login.show')
-        ->with('danger','ログアウトしました。');
+            ->with('danger', 'ログアウトしました。');
     }
 }
