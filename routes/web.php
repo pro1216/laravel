de13b006db385e8c1695b4fc5adac0328759372b
pro1216 <shotashota73@outlook.com>
2021-05-30 04:13:58 +0900

    ログインログアウト機能作成

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,23 @@ use App\Http\Controllers\Auth\AuthController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group(['middleware' => ['guest']],function(){
+    
+    //ログインホーム画面
+    Route::get('/',[AuthController::class,'showLogin'])
+    ->name('login.show');
+    //ログイン処理
+    Route::post('/login',[AuthController::class,'login'])
+    ->name('login');
+});
 
-Route::get('/',[AuthController::class,'showLogin'])
-->name('showLogin');
-
-Route::post('/login',[AuthController::class,'login'])
-->name('login');
+Route::group(['middleware'=>['auth']], function(){
+//ホーム画面
+Route::get('home',function(){
+    return view('home');
+ })->name('home');
+ //ログアウト
+ Route::post('logout',
+ [AuthController::class, "logout"]
+)->name('logout');
+});
